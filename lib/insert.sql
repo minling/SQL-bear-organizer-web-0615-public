e625a8e917ae8cc8579959bad51bb2a2451bac45
minling <mzhao725@gmail.com>
2015-06-17 14:32:16 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES 
+  ('Mr.Chocolate', 20, 'M', 'brown','sweet', 'TRUE'),
+  ('Rowdy', 28, 'M', 'Red', 'rowdy', 'TRUE'),
+  ('Tabitha', '12', 'F', 'Yellow', 'nice', 'TRUE'),
+  ('Sergeant Brown', 30, 'M', 'White', 'Strict', 'TRUE'),
+  ('Melissa', 13, 'F', 'Pink', 'nice', 'TRUE'),
+  ('Grinch', 99, 'M', 'Green', 'mean', 'TRUE'),
+  ('Wendy', 5, 'F', 'Yellow', 'nice', 'TRUE'),
+  (NULL, 50, 'M', 'Brown', 'mean', 'FALSE');
